8e6cda32b1314a87fd359426cee3181f77b95097
jakpm <jcalles@kpmg.es>
2017-01-19 23:11:07 +0100

    Separacion funciones tratamiento IPs

--- FluScan.py
+++ FluScan.py
@@ -1,7 +1,7 @@
 import socket
 import pygeoip
 from ports import getcommonports, portscan
-from ipaddress import ip_private
+from ipaddress import ip_private, ip_add, ip_order
 from dbconnect import Dbconnect 
 
 def geo(_file, _ip, _id):
@@ -38,38 +38,7 @@ def ports(_ip, _id):
     except:
         pass
 
-def ip_order(_ip1, _ip2):
-    try:
-        _ip1_split = _ip1.split('.')
-        _ip2_split = _ip2.split('.')
-        for i in range(0, 4):
-            if _ip2_split[i] < _ip1_split[i]:
-                return _ip1, _ip2
-            elif _ip2_split[i] > _ip1_split[i]:
-                return _ip2, _ip1
-        return _ip2, _ip1
-    except:
-        return _ip1, _ip2
-
-def ip_add(_ip):
-    try:
-        _ip_split = _ip.split('.')
-        if int(_ip_split[3]) < 255:
-            return "%s.%s.%s.%d" % (_ip_split[0], _ip_split[1], _ip_split[2], int(_ip_split[3])+1)
-        elif int(_ip_split[2]) < 255:
-            return "%s.%s.%d.%d" % (_ip_split[0], _ip_split[1], int(_ip_split[2])+1, 0)
-        elif int(_ip_split[1]) < 255:
-            return "%s.%d.%d.%d" % (_ip_split[0], int(_ip_split[1])+1, 0, 0)
-        elif int(_ip_split[0]) < 255:
-            return "%d.%d.%d.%d" % (int(_ip_split[0])+1, 0, 0, 0)
-        else:
-            return _ip
-    except:
-        return _ip
-
-
-
-def main(_ip1,_ip2):
+def main(_ip1, _ip2):
     ''' Main function, launch the main activities '''
     _ip3 = _ip1
     _ip3_prev = ""
--- ipaddress.py
+++ ipaddress.py
@@ -13,3 +13,32 @@ def ip_private(_ip):
         if (ip & addr[1]) == addr[0]:
             return True
     return False
+
+def ip_order(_ip1, _ip2):
+    try:
+        _ip1_split = _ip1.split('.')
+        _ip2_split = _ip2.split('.')
+        for i in range(0, 4):
+            if _ip2_split[i] < _ip1_split[i]:
+                return _ip1, _ip2
+            elif _ip2_split[i] > _ip1_split[i]:
+                return _ip2, _ip1
+        return _ip2, _ip1
+    except:
+        return _ip1, _ip2
+
+def ip_add(_ip):
+    try:
+        _ip_split = _ip.split('.')
+        if int(_ip_split[3]) < 255:
+            return "%s.%s.%s.%d" % (_ip_split[0], _ip_split[1], _ip_split[2], int(_ip_split[3])+1)
+        elif int(_ip_split[2]) < 255:
+            return "%s.%s.%d.%d" % (_ip_split[0], _ip_split[1], int(_ip_split[2])+1, 0)
+        elif int(_ip_split[1]) < 255:
+            return "%s.%d.%d.%d" % (_ip_split[0], int(_ip_split[1])+1, 0, 0)
+        elif int(_ip_split[0]) < 255:
+            return "%d.%d.%d.%d" % (int(_ip_split[0])+1, 0, 0, 0)
+        else:
+            return _ip
+    except:
+        return _ip
